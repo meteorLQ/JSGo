d68518cb74dae9fbffc0e2ad9ba41b6c8dc7eac5
shadowLQ <415804410@qq.com>
2021-10-04 16:39:19 +0800

    ref函数

---
+++ Vue/day6/vue3_test/src_初识setup/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>我是APP组件</h1>
+  <h2>name:{{name}}</h2>
+  <h2>age:{{age}}</h2>
+  <button @click="sayHello">说话</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  setup(){
+    // 数据
+    let name='张三'
+    let age=18
+    // 方法
+    function sayHello() {
+      alert(`${name}`)
+    }
+    return{name,age,sayHello}
+  }
+}
+</script>
+
