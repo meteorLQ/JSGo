6a9e5822ef70c594cc9478a5eaa201129a93b390
lq <shadowsLQ@126.com>
2021-11-03 22:23:49 +0800

    登录loginAPI

--- rzb-ui/src/api/user.js
+++ rzb-ui/src/api/user.js
@@ -2,8 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    baseURL:'/dev-api',
-    url: '/vue-admin-template/user/login',
+    url: '/login',
     method: 'post',
     data
   })
@@ -11,8 +10,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    baseURL:'/dev-api',
-    url: '/vue-admin-template/user/info',
+    url: '/info',
     method: 'get',
     params: { token }
   })
@@ -20,8 +18,7 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    baseURL:'/dev-api',
-    url: '/vue-admin-template/user/logout',
+    url: '/user/logout',
     method: 'post'
   })
 }
