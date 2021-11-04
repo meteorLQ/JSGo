b7e551735c95e7acbdffed5d465630e2498de969
lq <shadowsLQ@126.com>
2021-11-04 23:36:02 +0800

    登录loginAPI

--- rzb-ui/src/api/user.js
+++ rzb-ui/src/api/user.js
@@ -1,10 +1,10 @@
 import request from '@/utils/request'
 
-export function login(data) {
+export function login(LoginUser) {
   return request({
     url: '/login',
     method: 'post',
-    data
+    data: LoginUser
   })
 }
 
