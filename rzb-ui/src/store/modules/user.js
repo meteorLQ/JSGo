82897d2bd1d7cc80ea8dc2844191abcd2c00da2c
shadowLQ <415804410@qq.com>
2021-11-11 23:12:46 +0800

    token

--- rzb-ui/src/store/modules/user.js
+++ rzb-ui/src/store/modules/user.js
@@ -32,14 +32,14 @@ const mutations = {
 }
 
 const actions = {
-  // user login
+  // 登录
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
-        //commit('SET_TOKEN', data.token)
-        //setToken(data.token)
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
         resolve()
       }).catch(error => {
         reject(error)
