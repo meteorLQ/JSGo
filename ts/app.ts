7c5c0b960a52950af9190dd9636cc8174f22f4b6
shadowLQ <415804410@qq.com>
2021-11-30 18:26:45 +0800

    TS测试

---
+++ ts/app.ts
@@ -0,0 +1,4 @@
+var message:string ="hello world"
+let foo = 1;
+
+console.log(message)
---
+++ ts/hello.ts
@@ -0,0 +1,7 @@
+function sayHello(person: string) {
+    return 'hello, ' + person;
+}
+
+let user = 'Tom';
+let flag: boolean = true;
+console.log(sayHello(user))
