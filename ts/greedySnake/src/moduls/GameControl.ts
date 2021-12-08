68c3dc531f7b732d31ed093cc96e55fee56f56ab
shadowLQ <415804410@qq.com>
2021-12-08 23:30:29 +0800

    snake完结

--- ts/greedySnake/src/moduls/GameControl.ts
+++ ts/greedySnake/src/moduls/GameControl.ts
@@ -45,18 +45,34 @@ class GameControl {
                 x -= 10;
                 break;
         }
+        this.checkEat(x, y)
         try {
             this.snake.X = x;
             this.snake.Y = y;
-        }catch (e:any) {
-          //  alert(e.message)
-            alert(e.message)
-            this.isLive=false;
+        } catch (e: any) {
+            //  alert(e.message)
+            alert(e.message + 'Game over');
+            this.isLive = false;
         }
 
 
         this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
     }
+
+
+    // 定义一个方法，用来检查蛇是否吃到食物
+    checkEat(x: number, y: number) {
+        if (x === this.food.X && y === this.food.Y) {
+            console.log('吃到食物了')
+            // 食物的位置重置
+            this.food.change();
+            // 分数增加
+            this.scorePanel.addScore();
+            // 蛇增加一节
+            this.snake.addBody();
+        }
+    }
+
 }
 
 export default GameControl;
--- ts/greedySnake/src/moduls/Snake.ts
+++ ts/greedySnake/src/moduls/Snake.ts
@@ -24,10 +24,25 @@ class Snake {
         if (this.X === value) {
             return
         }
+
+
         if (value < 0 || value > 290) {
             throw new Error('游戏结束！');
         }
+
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            if (value > this.X) {
+                value = this.X - 10;
+            } else {
+                value = this.X + 10;
+            }
+        }
+
+        // 移动身体
+        this.moveBody();
         this.head.style.left = value + 'px';
+        this.checkHeadBody();
+
     }
 
     set Y(value) {
@@ -37,12 +52,49 @@ class Snake {
         if (value < 0 || value > 290) {
             throw new Error('游戏结束！');
         }
+
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            if (value > this.Y) {
+                value = this.Y - 10;
+            } else {
+                value = this.Y + 10;
+            }
+        }
+
+
+        // 移动身体
+        this.moveBody();
+
         this.head.style.top = value + 'px';
+        this.checkHeadBody();
     }
 
     addBody() {
         this.element.insertAdjacentHTML("beforeend", "<div></div>");
     }
+
+    // 添加一个蛇身体移动的方法
+    moveBody() {
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+            (this.bodies[i - 1] as HTMLElement).style.left = X + 'px';
+            (this.bodies[i - 1] as HTMLElement).style.top = Y + 'px';
+        }
+    }
+
+    checkHeadBody(){
+        // 获取所有的身体，检查其是否和蛇头的坐标发生重叠
+        for (let i = 0; i < this.bodies.length; i++) {
+            let body = this.bodies[i] as  HTMLElement;
+            if (this.X===body.offsetLeft && this.Y===body.offsetTop){
+                throw new Error('撞到自己了～～～～～～～')
+            }
+            
+        }
+
+    }
+
 }
 
 export default Snake;
