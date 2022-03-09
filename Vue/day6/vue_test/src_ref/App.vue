123cdbcd74874265590c81c23a3fc847769daa6d
shadowLQ <415804410@qq.com>
2022-03-09 11:14:06 +0800

    feat:ref

---
+++ Vue/day6/vue_test/src_ref/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <Student ref="stu"></Student>
+    <hr>
+    <School></School>
+    <div ref="hi">你好,欢迎学习ref</div>
+    <button @click="showRef">点击</button>
+  </div>
+</template>
+
+<script>
+import Student from "./components/Student";
+import School from "./components/School";
+
+export default {
+  name: 'App',
+  components: {
+    School, Student
+  }, methods: {
+    showRef(){
+      console.log(this.$refs.stu)
+      console.log(this.$refs.hi)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/day6/vue_test/src_ref/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    studentName:{{studentName}}
+    <br>
+    studentAge:{{studentAge}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      studentName: '擎天柱',
+      studentAge: '110'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
