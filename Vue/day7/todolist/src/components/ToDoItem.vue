c4b354452ac1ecfd1f7061e08bb3376ea5d7a12f
shadowLQ <415804410@qq.com>
2022-03-09 18:18:51 +0800

    feat:todolist

---
+++ Vue/day7/todolist/src/components/ToDoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "ToDoItem"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/day7/todolist/src/components/ToDoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="todo-main">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        <span>xxxxx</span>
+      </label>
+      <button class="btn btn-danger" style="display:none">删除</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+        <span>yyyy</span>
+      </label>
+      <button class="btn btn-danger" style="display:none">删除</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem";
+
+export default {
+  name: "ToDoList",
+  comments: {
+    ToDoItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
