b6f98b18a4eab4bea5fb54b1fac9991ff6c98051
shadowLQ <415804410@qq.com>
2022-03-10 15:49:34 +0800

    featD:组件自定义事件

---
+++ Vue/day7/todolist/src_组件自定义事件/components/ToDoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="todo-main">
+
+    <ToDoItem v-for="todo in todos" :key="todo.id" :todoO="todo" :checkToDo="checkToDo" :deleteToDoById="deleteToDoById"/>
+  </ul>
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem";
+
+export default {
+  name: "ToDoList",
+  components: {ToDoItem},
+  props:['todos','checkToDo','deleteToDoById'],
+  comments: {
+    ToDoItem
+  }
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
