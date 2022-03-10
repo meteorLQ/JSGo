532adccc9bfd1e26dc3860344c043932296b17f1
shadowLQ <415804410@qq.com>
2022-03-10 18:41:59 +0800

    feat:todo编辑

---
+++ Vue/day7/todolist/vue.config.js
@@ -0,0 +1,14 @@
+module.exports={
+  pages:{
+    index:{
+      // 入口
+      entry:'src/main.js',
+    },
+  },
+  // 关闭eslint语法检测
+  lintOnSave: false
+  // 开启代理服务器
+  // devServer: {
+  //   proxy: 'http://localhost:9090'
+  // }
+}
