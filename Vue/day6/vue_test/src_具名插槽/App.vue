99afdec714c3ffb3960a5078f7e2a7d2fbf3cb6c
shadowLQ <415804410@qq.com>
2022-03-11 11:08:14 +0800

    feat:具名插槽

---
+++ Vue/day6/vue_test/src_具名插槽/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <Category title="美食">
+      <img slot="center" src="https://t7.baidu.com/it/u=2168645659,3174029352&fm=193&f=GIF" alt="">
+      <a slot="foot" href="">更多</a>
+    </Category>
+    <Category title="游戏">
+      <ul slot="center">
+        <li v-for="(d,index) in games" :key="index">{{ d }}</li>
+      </ul>
+     <template v-slot:foot>
+       <a  href="">单机</a>
+       <a  href="">网络</a>
+     </template>
+    </Category>
+    <!--    <Categor  y title="电影" :listData="films"></Category>-->
+  </div>
+</template>
+
+<script>
+
+
+import Category from "./components/Category";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      foods: ['牛奶', '面包', '可乐'],
+      games: ['法环', '红警', '师父'],
+      films: ['指环王1', '指环王2', '指环王3']
+    }
+  },
+  components: {
+    Category
+  }
+}
+</script>
+
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+
+</style>
---
+++ Vue/day6/vue_test/src_具名插槽/components/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <slot name="center"></slot>
+    <slot name="foot"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props:['listData','title']
+}
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3{
+  text-align: center;
+  background-color: orange;
+}
+img{
+  width: 100%;
+}
+</style>
