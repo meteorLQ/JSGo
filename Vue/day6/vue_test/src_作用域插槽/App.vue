30e1b7c1faba6b82297e7104fc2856f6d802a256
shadowLQ <415804410@qq.com>
2022-03-11 13:55:02 +0800

    feat:作用域插槽

---
+++ Vue/day6/vue_test/src_作用域插槽/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <Category title="游戏">
+      <template v-slot="data">
+        <ul>
+          <li v-for="(d,index) in data.games" :key="index">{{ d }}</li>
+        </ul>
+      </template>
+
+    </Category>
+    <Category title="游戏">
+      <template v-slot="data">
+        <ol>
+          <li v-for="(d,index) in data.games" :key="index">{{ d }}</li>
+        </ol>
+      </template>
+
+    </Category>
+    <Category title="游戏">
+      <template v-slot="data">
+        <h4 v-for="(d,index) in data.games" :key="index">{{ d }}</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<script>
+
+
+import Category from "./components/Category";
+
+export default {
+  name: 'App',
+  components: {
+    Category
+  }
+}
+</script>
+
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+
+</style>
---
+++ Vue/day6/vue_test/src_作用域插槽/components/Category.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <slot :games="games"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props:['title'],
+  data() {
+    return {
+      games: ['法环', '红警', '师父']
+    }
+  },
+}
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3{
+  text-align: center;
+  background-color: orange;
+}
+img{
+  width: 100%;
+}
+</style>
